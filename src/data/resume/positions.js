e1c38f87bc9f77cea709816da8dfb49c9a7993c2
SimonJuba <scimail09@gmail.com>
2022-04-03 02:25:03 +0900

    update positions

--- src/data/resume/positions.js
+++ src/data/resume/positions.js
@@ -5,9 +5,23 @@ const positions = [
     link: 'https://reqrea.co.jp/',
     daterange: 'December 2021 - Present',
     points: [
-      'A Startup based in Fukuoka City, Japan. Focused on modernising properties',
+      'A Startup based in Fukuoka City, Japan. Focused on modernizing properties',
       'Combating the lack of staff available to work in remote hotels by automating the check in process',
-      'Involved in the development and operation of their inhouse built hotel check in system',
+      'Working as a part of a 5 developer team working on the maintenance and development of SAAS hotel check-in system',
+      'Experience with web app development in PHP with Laravel',
+      'Experience with mobile app development in Swift',
+    ],
+  },
+  {
+    company: 'HENNGE',
+    position: 'Software Engineering Intern',
+    link: 'https://hennge.com/global/gip/remote.html',
+    daterange: 'January - February 2022',
+    points: [
+      'A Cloud Computing company based in Tokyo, Japan. Joined their Global Internship Program.',
+      'Hands on experience with full-stack development using Go and React with Typescript.',
+      'Hands on experience with Docker containerization, zero-downtime deployment with AWS ECS and ALB.',
+      'Deployed and managed cloud Infrastructure on AWS using Terraform.',
     ],
   },
   {
@@ -16,10 +30,9 @@ const positions = [
     link: 'https://www.team-lab.com/',
     daterange: 'September 2021',
     points: [
-      'Joined their summer internship program which aims to equip interns with basic web development skills',
-      'Designed and developed a RESTful API satisfying pre-defined requirements',
-      'Maily used the Java Spring boot framework',
-      'Implemented OAuth based login and token based authentication',
+      'Joined their summer internship program which aims to equip interns with basic Web Development skills',
+      'Hands on experience with web app development using the Spring Boot framework',
+      'Hands on experience with API and database design, authentication, authorization, and custom logging implementations',
     ],
   },
 ];
